7bdacd11b9205c9085e14d9f516fcbdc38f2e831
Vicente Manriquez <vicente.manriquez@sansano.usm.cl>
2019-10-23 18:49:01 -0300

    [UPDATE] actualizao cliente y servidor avance en  recibir mensajes, atributos self. se comparten entre threads

--- rpc/client.py
+++ rpc/client.py
@@ -1,25 +1,26 @@
 import grpc
 import Chat_pb2,Chat_pb2_grpc
 import threading
+import time 
 
 class Client():
     def __init__(self,nombre):
         self.nombre = nombre 
         self.channel = grpc.insecure_channel('localhost:8080')
         self.stub = Chat_pb2_grpc.ChatStub(self.channel)
-        self.id = self.stub.Connection(Chat_pb2.Nombre(nombre = 'nombre')).id # se le pide al server que nos de un id 
+        self.id = self.stub.Connection(Chat_pb2.Nombre(nombre = nombre)).id # se le pide al server que nos de un id 
         self.my_user = Chat_pb2.User(
             id = self.id,
             nombre = self.nombre 
         )
         #se crea thread para que escuche los mensajes entrantes 
-        threading.Thread(target=self.ReciveMessage).start()
+        threading.Thread(target=self.ReciveMessage,daemon=True).start()
 
 
     def Ping(self):
         respuesta = self.stub.Ping(
         Chat_pb2.Pong(
-            ping = 'Miedo Potter ? '
+            ping = self.nombre
             )
         )
         print(respuesta)
@@ -31,19 +32,26 @@ class Client():
                 emisor = self.my_user,
                 contenido = "miedo potter ? ",
                 timestamp = "intento",
-                id = 3,
                 receptor = Chat_pb2.User(
                     id = 3 ,#que aca sea estilo destino.id
-                    nombre = 'jiji' #destino.nombre o algo asi 
+                    nombre = 'receptor' #destino.nombre o algo asi 
                 )
             ))
         except grpc.RpcError as err:
             print(err)
 
     def ReciveMessage(self):
-        for mensaje in self.stub.ReciveMessage(Chat_pb2.Empty()):
-            emisor = mensaje.emisor
-            print(f'[{emisor.nombre}#{emisor.id}-{mensaje.timestamp}]{mensaje.contenido}')
+        try:
+            for mensaje in self.stub.ReciveMessage(Chat_pb2.Empty()):
+                emisor = mensaje.emisor
+                print(f'[{emisor.nombre}#{emisor.id}-{mensaje.timestamp}]{mensaje.contenido}')
+        except grpc.RpcError as err:
+            print(err)
+print('Ingrese su nombre de usuario: ',end = '')
+nombre = input()
 
-client = Client('Vicente ')
-client.Ping()
+client = Client(nombre)
+client.SendMessage('contenido','vicente')
+while True:
+    client.Ping()
+    time.sleep(10)
--- rpc/server.py
+++ rpc/server.py
@@ -3,35 +3,43 @@ import grpc
 import time 
 import Chat_pb2
 import Chat_pb2_grpc
+import threading
 
 class Server(Chat_pb2_grpc.ChatServicer):
 
     def __init__(self):
         self.user_id = 0
         self.message_id = 0
-
+        self.enviados = {}
+        self.nombre = []
     def Connection(self,request,context):
-        nombre = request.nombre # se obtiene el nombre de la persona que hizo la request, supongo que despue slo ssamos ? 
+        print(f'Responde el thread{threading.get_ident()}')
+        self.nombre.append(request.nombre) # se obtiene el nombre de la persona que hizo la request, supongo que despue slo ssamos ? 
         new_id = self.user_id
-
+        self.enviados[new_id] = [] # se crea llave = id , valor = lista de mensajes enviados  
         self.user_id+=1
         return Chat_pb2.Id(id = new_id)
     
     def SendMessage(self,message,context):
         contenido = message.contenido
         print(f'El contendio del mensaje es: {contenido }')
+        print(f'Este es mi nombre: {self.nombre}')
         return Chat_pb2.Empty()
 
     def Ping(self,pong,context):
         respuesta = pong.ping
-        print(f'El cliente envio {respuesta}')
+        print(f'El cliente {respuesta} tiene estos nombres: {self.nombre}')
         return Chat_pb2.Pong(
             ping = 'Ni un Poco '
         )
 
-    def ReciveMessage(self,context):
+    def ReciveMessage(self,request_iterator,context):
+        
         while True:
-            pass
+            time.sleep(10)
+            yield Chat_pb2.Message(
+                contenido = "wena"
+            )
 
 if __name__ == "__main__":
     #Se corre server con n threads

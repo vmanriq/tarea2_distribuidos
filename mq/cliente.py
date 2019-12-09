434c77a46104beafecdd82c53cc00219c13a2e91
manueljesusmatus <manueljesusmatus@gmail.com>
2019-12-09 05:06:00 -0300

    [UPDATE] TCP + RABBITmq

--- mq/cliente.py
+++ mq/cliente.py
@@ -1,42 +1,54 @@
 import pika
 import threading
+import socket
+
 
 RABBIT = 'localhost'
+HOST = '0.0.0.0'
+PORT = 5050
+
 
 class Cliente:
     """docstring for Cliente."""
-    def __init__(self, nombre, id):
+    def __init__(self, nombre):
+        # INIT
         self.nombre = nombre
-        self.id = id
-        #self.id = get_id()
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.connect((HOST, PORT))
+        self.s.sendall(b'Iniciando Conexion')
+
+        # RECIBE EL ID
+        data = self.s.recv(1024)
+        self.id = int(data.decode("utf-8"))
+
+        # Se declara la cola
         connection =  pika.BlockingConnection(pika.ConnectionParameters(RABBIT))
         self.channel = connection.channel()
-        ###
-        self.channel.queue_declare(queue= f'send#{self.id}')
         self.channel.queue_declare(queue= f'recive#{self.id}')
         threading.Thread(target=self.recive_message, daemon=True).start()
-        #self.channel.queue_declare(exchange='send', queue= self.id)
+
         print(f'mi id es {self.id}')
+
     def callback(self, ch, method, properties, body):
         print(" [x] %r" % body)
 
     def send_message(self, receptor, message):
-        self.channel.basic_publish(exchange='', routing_key=f'recive#{receptor}',
-                                    body=message)
+        # Darle formato JSON/Diccionario
+        msn = receptor+"#"+mensaje
+        self.s.sendall(msn.encode())
+        #self.channel.basic_publish(exchange='', routing_key=f'send#{receptor}',body=message)
 
     def recive_message(self):
         connection =  pika.BlockingConnection(pika.ConnectionParameters(RABBIT))
         channel = connection.channel()
         channel.basic_consume(queue=f'recive#{self.id}', on_message_callback=self.callback, auto_ack=True)
-        #print("WENA CARBOS ESTE EL THREAD \n \n \n")
         channel.start_consuming()
 
 
 
 if __name__ == "__main__":
     nombre = input("Ingrese su nombre: ")
-    id = input("INgrese id: ")
-    cliente = Cliente(nombre, id )
+    cliente = Cliente(nombre)
     while True:
         print("Ingrese mensaje a enviar: ")
         mensaje = input()
--- mq/server.py
+++ mq/server.py
@@ -1,31 +1,50 @@
 import pika
 import socket
+import threading
 
 from concurrent import futures
 
-PORT = 5000
+PORT = 5050
 HOST = "0.0.0.0"
+RABBIT = 'localhost'
 
 class ClientHanlder():
-    def __init__(self,conn,addr):
-        self.user_id = 0
-        print("Se conecto:",addr)
-        with conn:
-            self.principal()
-            data = conn.recv(1024)
-            conn.sendall(b"Se recibio correctamente la peticion")
-            registro.write(str(addr[0])+":"+repr(data)+"\n")
-            registro.close()
-    def principal(self):
-        print("funciono")
+    def __init__(self,conn,addr,id):
+        # INIT
+        self.id = id
+        self.conn = conn
+        data = self.conn.recv(1024)
+
+        # ENVIA EL ID ?¡
+        self.conn.sendall(str(id).encode())
+
+        # CREA LAS COLAS
+        connection =  pika.BlockingConnection(pika.ConnectionParameters(RABBIT))
+        self.channel = connection.channel()
+        self.channel.queue_declare(queue= f'recive#{self.id}')
+        threading.Thread(target=self.recive_message, daemon=True).start()
+
+    def recive_message(self):
+        while True:
+            data = self.conn.recv(1024)
+            self.send_message(self, data)
+
+
+    def send_message(self, C ,body):
+        # Esta weaita sería el decode del JSON/Diccionario
+        message = body.decode("utf-8").split("#")
+        self.channel.basic_publish(exchange='', routing_key=f'recive#{message[0]}',
+                                    body=message[1])
         return
 
+
 if __name__ == "__main__":
+    IDS = 1
     s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     s.bind((HOST,PORT))
     s.listen()
     while True:
         conn, addr = s.accept()
-
         server  = futures.ThreadPoolExecutor(max_workers=10)
-        f = server.submit(ClientHanlder,args=(conn,addr,))
+        f = server.submit(ClientHanlder(conn,addr,IDS))
+        IDS+=1

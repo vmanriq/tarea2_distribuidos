96e99e501f103038d07564ea18dd2567615d7149
manueljesusmatus <manueljesusmatus@gmail.com>
2019-12-22 01:59:07 -0300

    [INIT] First try

--- mq/cliente.py
+++ mq/cliente.py
@@ -1,45 +1,116 @@
 import pika
 import threading
+import socket
+import json
+from datetime import datetime
+import sys
 
+HOST = 'localhost'
 RABBIT = 'localhost'
+PORT = 5062
+
 
 class Cliente:
     """docstring for Cliente."""
-    def __init__(self, nombre, id):
+    def __init__(self, nombre):
+        # INIT
         self.nombre = nombre
-        self.id = id
-        #self.id = get_id()
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.connect((HOST, PORT))
+        self.s.sendall(bytes(self.nombre, 'utf-8'))
+
+        # RECIBE EL ID
+        data = self.s.recv(1024)
+        self.id = int(data.decode("utf-8"))
+
+        # Se declara la cola
         connection =  pika.BlockingConnection(pika.ConnectionParameters(RABBIT))
         self.channel = connection.channel()
-        ###
-        self.channel.queue_declare(queue= f'send#{self.id}')
         self.channel.queue_declare(queue= f'recive#{self.id}')
         threading.Thread(target=self.recive_message, daemon=True).start()
-        #self.channel.queue_declare(exchange='send', queue= self.id)
-        print(f'mi id es {self.id}')
-    def callback(self, ch, method, properties, body):
-        print(" [x] %r" % body)
 
-    def send_message(self, receptor, message):
-        self.channel.basic_publish(exchange='', routing_key=f'recive#{receptor}',
-                                    body=message)
+        print(f'mi id es {self.id}')
+
+    def callback(self, ch, method, properties, body):
+        message = json.loads(body.decode('utf-8'))
+        if(message['tipo']==0):
+            print(f"[{message['time']}] {message['nombre_emisor']}#{message['id_emisor']} : {message['body']}")
+        elif(message['tipo']==1):
+            if(len(message['body'])==0):
+                print(f'No existen mensajes enviados')
+            else:
+                for i in message['body']:
+                    print(f"[{i['time']}] {i['nombre_emisor']}#{i['id_emisor']} to {i['nombre_receptor']}#{i['id_receptor']} : {i['body']}")
+        elif(message['tipo']==2):
+            for i in message['body']:
+                print(f'->{i}')
+        else:
+            print(f"{message['body']}")
+        print('Formato mensaje : !msn:{detinatario}#{id}:{mensaje}')
+        print('Formato comando listado : !listado')
+        print('formato comando mensajes enviados : !mensajes')
+        print('>> Ingrese accion: '),
+        
+
+    # comando e {0,1,2} : 0 = send_message ; 1 == historial ; 2==list_user
+    def send_message(self, receptor, message,comando ): 
+        # Darle formato JSON/Diccionario
+        nombre,id = receptor.split("#")
+        mensaje = {}
+        now = datetime.now()
+        if comando == 0:
+            mensaje = {
+                    'tipo' : 0,
+                    'body' : message,
+                    'id_receptor' : id,
+                    'nombre_receptor' : nombre,
+                    'id_emisor' : self.id,
+                    'nombre_emisor' : self.nombre,
+                    'time' : str(now.strftime("%d/%m/%Y %H:%M:%S"))
+            }
+        elif comando == 1:
+            mensaje = {
+                    'tipo' : 1,
+                    'comando' : 'historial'
+            }
+        elif comando == 2:
+            mensaje = {
+                    'tipo' : 2,
+                    'comando' : 'list_user'
+            }
+
+        msn = json.dumps(mensaje)
+        self.s.sendall(msn.encode())
 
     def recive_message(self):
         connection =  pika.BlockingConnection(pika.ConnectionParameters(RABBIT))
         channel = connection.channel()
         channel.basic_consume(queue=f'recive#{self.id}', on_message_callback=self.callback, auto_ack=True)
-        #print("WENA CARBOS ESTE EL THREAD \n \n \n")
         channel.start_consuming()
+    
+
 
 
 
 if __name__ == "__main__":
     nombre = input("Ingrese su nombre: ")
-    id = input("INgrese id: ")
-    cliente = Cliente(nombre, id )
+    cliente = Cliente(nombre)
+    print('Formato mensaje : !msn:{detinatario}#{id}:{mensaje}')
+    print('Formato comando listado : !listado')
+    print('formato comando mensajes enviados : !mensajes')
+    print('>> Ingrese accion: ')
     while True:
-        print("Ingrese mensaje a enviar: ")
-        mensaje = input()
-        print("ingrese receptor: ")
-        receptor = input()
-        cliente.send_message(receptor, mensaje)
+        inp  = input()
+        ln = inp.split(':')
+        if((ln[0] == '!listado') and (len(ln)==1)):
+            cliente.send_message(f'{cliente.nombre}#{str(cliente.id)}', '!listado', 2)
+        elif((ln[0] == '!mensajes') and (len(ln)==1)):
+            cliente.send_message('#','!mensajes',1)
+        elif((ln[0] == '!salir') and (len(ln)==1)):
+            sys.exit()
+        elif((ln[0] == '!msn') and (len(ln)==3)):
+            destinatario = ln[1]
+            mensaje = ln[2]
+            cliente.send_message(destinatario, mensaje,0 )
+        else:
+            print('>> formato incorrecto, intente de nuevo')
--- mq/server.py
+++ mq/server.py
@@ -1,11 +1,131 @@
 import pika
+import socket
+import threading
+import json
 from concurrent import futures
 
-
-class Server():
+class id_message:
     def __init__(self):
-        self.user_id = 0
+        self.id = 0
+    def getIdMessage(self):
+        self.id += 1
+        return self.id
 
-if __name__ == "__main__"
+class Id:
+    def __init__(self):
+        self.id = 0
+    def update(self):
+        self.id+=1
+    def get(self):
+        return self.id 
 
-server  = futures.ThreadPoolExecutor(max_workers=10)
+class Users:
+    def __init__(self):
+        self.usuarios = []
+
+    def addUser(self, usuario):
+        self.usuarios.append(usuario)
+
+    def getUsers(self):
+        return self.usuarios
+
+PORT = 5062
+HOST = 'localhost'
+RABBIT = 'localhost'
+IDS = Id()
+USERS = Users()
+IDM = id_message() 
+
+class ClientHanlder():
+    def __init__(self, conn, addr, ID, USERS, IDM):
+        # INIT
+        self.id = ID.get()
+        self.IDM = IDM
+        print(f"Este es mi id {self.id}")
+        self.conn = conn
+        data = self.conn.recv(1024)
+        self.nombre = data.decode('utf-8')
+        print(f'Este es minombre {self.nombre}')
+        USERS.addUser(f'{self.nombre}#{str(self.id)}')
+        self.USERS = USERS
+        # ENVIA EL ID ?¡
+        self.conn.sendall(str(self.id).encode())
+
+        # CREA LAS COLAS
+        connection =  pika.BlockingConnection(pika.ConnectionParameters(RABBIT))
+        self.channel = connection.channel()
+        self.channel.queue_declare(queue= f'recive#{self.id}')
+        threading.Thread(target=self.recive_message, daemon=True).start()
+        #self.recive_message()
+
+    # comando e {0,1,2} : 0 = send_message ; 1 == historial ; 2 ==list_user
+    def recive_message(self):
+        while True:
+            # JSON CON EN BINARY STRING DESDE CLIENTE
+            data = self.conn.recv(1024)
+            # DESERIALIZE
+            message = json.loads(data.decode('utf-8'))
+            tipo = message['tipo']
+            if tipo == 0:
+                if self.verificar(message) == True:
+                    message['id_message'] = IDM.getIdMessage()
+                    a = open("log.txt","a")
+                    a.write(str(message)+"\n")
+                    a.close()
+                else:
+                    message = {
+                        'tipo' : 4,
+                        'body' : 'Error, el usuario ingresado no existe'
+                    }
+            elif tipo == 1:
+                message_list = []
+                a = open("log.txt","r")
+                for i in a:
+                    m = json.loads(i.replace("\'","\"").strip())
+                    if m['nombre_emisor'] == self.nombre :
+                        message_list.append(m)
+                a.close()
+                message = {
+                    'tipo' : 1,
+                    'body' : message_list
+                }                
+            elif tipo == 2:
+                message = {
+                    'tipo' : 2,
+                    'body' : self.USERS.getUsers(),
+                }    
+            self.send_message(message)
+
+    def send_message(self, message):
+        msn = json.dumps(message).encode()
+        try:
+            if(message['tipo']==0):
+                self.channel.basic_publish(exchange='', routing_key=f"recive#{message['id_receptor']}",
+                                    body=msn)
+            else:
+                self.channel.basic_publish(exchange='', routing_key=f"recive#{str(self.id)}",
+                                    body=msn)
+        except:
+            print(f"El usuario {message['nombre_emisor']} no existe")
+        return
+    
+    def verificar(self, message):
+        user = f"{message['nombre_receptor']}#{message['id_receptor']}"
+        if user in USERS.getUsers():
+            return True
+        else:
+            return False
+
+
+
+if __name__ == "__main__":
+    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s.bind((HOST,PORT))
+    s.listen()
+    #server  = futures.ThreadPoolExecutor(max_workers=10)
+    while True:
+        print(IDS.get())
+        server  = futures.ThreadPoolExecutor(max_workers=10)
+        conn, addr = s.accept()
+        server.submit(ClientHanlder(conn, addr, IDS, USERS, IDM))
+        IDS.update()

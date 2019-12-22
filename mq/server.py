f7e1ea6921498cfc4385c399427bbfcf69f91cbc
manueljesusmatus <manueljesusmatus@gmail.com>
2019-12-22 03:11:31 -0300

    [FIX] se arregla bug

--- mq/server.py
+++ mq/server.py
@@ -41,11 +41,9 @@ class ClientHanlder():
         # INIT
         self.id = ID.get()
         self.IDM = IDM
-        print(f"Este es mi id {self.id}")
         self.conn = conn
         data = self.conn.recv(1024)
         self.nombre = data.decode('utf-8')
-        print(f'Este es minombre {self.nombre}')
         USERS.addUser(f'{self.nombre}#{str(self.id)}')
         self.USERS = USERS
         # ENVIA EL ID ?¡
@@ -56,7 +54,6 @@ class ClientHanlder():
         self.channel = connection.channel()
         self.channel.queue_declare(queue= f'recive#{self.id}')
         threading.Thread(target=self.recive_message, daemon=True).start()
-        #self.recive_message()
 
     # comando e {0,1,2} : 0 = send_message ; 1 == historial ; 2 ==list_user
     def recive_message(self):
@@ -79,12 +76,15 @@ class ClientHanlder():
                     }
             elif tipo == 1:
                 message_list = []
-                a = open("log/log.txt","r")
-                for i in a:
-                    m = json.loads(i.replace("\'","\"").strip())
-                    if m['nombre_emisor'] == self.nombre :
-                        message_list.append(m)
-                a.close()
+                try:
+                    a = open("log/log.txt","r")
+                    for i in a:
+                        m = json.loads(i.replace("\'","\"").strip())
+                        if m['nombre_emisor'] == self.nombre :
+                            message_list.append(m)
+                    a.close()
+                except:
+                    print("No existen mensajes registrados")
                 message = {
                     'tipo' : 1,
                     'body' : message_list
@@ -122,9 +122,7 @@ if __name__ == "__main__":
     s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     s.bind((HOST,PORT))
     s.listen()
-    #server  = futures.ThreadPoolExecutor(max_workers=10)
     while True:
-        print(IDS.get())
         server  = futures.ThreadPoolExecutor(max_workers=10)
         conn, addr = s.accept()
         server.submit(ClientHanlder(conn, addr, IDS, USERS, IDM))
